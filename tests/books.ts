0d9c67d950e37d69a2ef2b179261ec6b990e3ab7
Oleh Solodkyi <truckman2895@gmail.com>
2024-05-05 20:55:55 +0300

    Small format change

--- tests/books.ts
+++ tests/books.ts
@@ -1,18 +1,17 @@
 export class Book {
-        protected title: string;
-        protected author: string;
-        protected year: number;
-    
-        public constructor(title: string, author: string, year: number) {
-            this.title = title;
-            this.author = author;
-            this.year = year;
-        }
-    
-        public getSummary(): string {
-            return `The book "${this.title}" was written in ${this.year} by ${this.author}.`;
-        }
-    }
-    const book1 = new Book("Harry Potter", "J.K. Rowling", 1997);
-    console.log(book1.getSummary());
-   
+  protected title: string;
+  protected author: string;
+  protected year: number;
+
+  public constructor(title: string, author: string, year: number) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  public getSummary(): string {
+    return `The book "${this.title}" was written in ${this.year} by ${this.author}.`;
+  }
+}
+const book1 = new Book("Harry Potter", "J.K. Rowling", 1997);
+console.log(book1.getSummary());
--- tests/e-books.ts
+++ tests/e-books.ts
@@ -1,17 +1,16 @@
-import {Book} from "./books"
+import { Book } from "./books";
 
-class ElectronicBook extends Book{
-    protected format: string;
+class ElectronicBook extends Book {
+  protected format: string;
 
-    constructor(title: string, author: string, year: number, format: string) {
-        super(title, author, year);
-        this.format = format;
-       
-    }
+  constructor(title: string, author: string, year: number, format: string) {
+    super(title, author, year);
+    this.format = format;
+  }
 
-    public getSummary(): string {
-        return `The "${this.title}" e-book is available in ${this.format}.`;
-    }
+  public getSummary(): string {
+    return `The "${this.title}" e-book is available in ${this.format}.`;
+  }
 }
 const book1 = new ElectronicBook("Harry Potter", "J.K. Rowling", 1997, "PDF");
 console.log(book1.getSummary());
